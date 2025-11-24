94b01e041d5338b59439d39303da4782d6e4d190
“Vinicius <viniciussilva707@gmail.com>
2025-11-24 07:15:32 -0300

    Enhance API by adding functions to fetch and create transactions, and update type imports to include transaction models.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { IUsuario } from "../types";
+import { ITransacoes, IUsuario } from "../types";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
@@ -14,5 +14,17 @@ export const criarUsuario = async (
   usuario: Omit<IUsuario, "id">
 ): Promise<IUsuario> => {
   const { data } = await api.post<IUsuario>("/usuarios", usuario);
+  return data;
+};
+
+export const obterTransacoes = async (): Promise<ITransacoes[]> => {
+  const { data } = await api.get<ITransacoes[]>("/transacoes");
+  return data;
+};
+
+export const criarTransacao = async (
+  transacao: Omit<ITransacoes, "id">
+): Promise<ITransacoes> => {
+  const { data } = await api.post<ITransacoes>("/transacoes", transacao);
   return data;
 };
