9f744a8ebafcb1b6839119136c17a4eee3a3a790
“Vinicius <viniciussilva707@gmail.com>
2025-11-24 07:10:32 -0300

    Update db.json to rename "usuario" to "usuarios" and enhance API with functions to fetch and create users.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
+import { IUsuario } from "../types";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
 });
+
+export const obterUsuario = async (): Promise<IUsuario[]> => {
+  const { data } = await api.get<IUsuario[]>("/usuarios");
+  return data;
+};
+
+export const criarUsuario = async (
+  usuario: Omit<IUsuario, "id">
+): Promise<IUsuario> => {
+  const { data } = await api.post<IUsuario>("/usuarios", usuario);
+  return data;
+};
