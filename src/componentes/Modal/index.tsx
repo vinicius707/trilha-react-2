8ba7b307c008ab26ad5025785c6382cc62dbd9c5
“Vinicius <viniciussilva707@gmail.com>
2025-11-24 06:18:22 -0300

    Refactor Modal component to use forwardRef and simplify props. Added peer dependencies in package-lock.json.

--- src/componentes/Modal/index.tsx
+++ src/componentes/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { forwardRef, useRef } from "react";
 import {
   ButtonGroup,
   CloseButton,
@@ -11,68 +11,40 @@ import Botao from "../Botao";
 interface ModalProps {
   icon: React.ReactNode;
   titulo: string;
-  aoFechar: () => void;
   children: React.ReactNode;
-  estaAberta: boolean;
   aoClicar: () => void;
 }
 
-const Modal = ({
-  icon,
-  titulo,
-  aoFechar,
-  children,
-  estaAberta,
-  aoClicar,
-}: ModalProps) => {
-  const dialogRef = useRef<HTMLDialogElement>(null);
+export interface ModalHandle {
+  open: () => void;
+  close: () => void;
+}
 
-  useEffect(() => {
-    const dialogNode = dialogRef.current;
+const Modal = forwardRef<ModalHandle, ModalProps>(
+  ({ icon, titulo, children, aoClicar }, ref) => {
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
-    if (dialogNode) {
-      if (estaAberta) {
-        dialogNode.showModal();
-      } else {
-        dialogNode.close();
-      }
-    }
-
-    const handleClose = () => aoFechar();
-
-    if (dialogNode) {
-      dialogNode.addEventListener("close", handleClose);
-    }
-
-    return () => {
-      if (dialogNode) {
-        dialogNode.removeEventListener("close", handleClose);
-      }
-    };
-  }, [estaAberta, aoFechar]);
-
-  return (
-    <ModalOverlay>
-      <ModalContainer ref={dialogRef}>
-        <ModalHeader>
-          <div>
-            {icon}
-            {titulo}
-          </div>
-          <CloseButton onClick={aoFechar}>x</CloseButton>
-        </ModalHeader>
-        {children}
-        <ButtonGroup>
-          <Botao $variante="secundario" onClick={aoFechar}>
-            Cancelar
-          </Botao>
-          <Botao $variante="primario" onClick={aoClicar}>
-            Adicionar
-          </Botao>
-        </ButtonGroup>
-      </ModalContainer>
-    </ModalOverlay>
-  );
-};
+    return (
+      <ModalOverlay>
+        <ModalContainer ref={dialogRef}>
+          <ModalHeader>
+            <div>
+              {icon}
+              {titulo}
+            </div>
+            <CloseButton>x</CloseButton>
+          </ModalHeader>
+          {children}
+          <ButtonGroup>
+            <Botao $variante="secundario">Cancelar</Botao>
+            <Botao $variante="primario" onClick={aoClicar}>
+              Adicionar
+            </Botao>
+          </ButtonGroup>
+        </ModalContainer>
+      </ModalOverlay>
+    );
+  }
+);
 
 export default Modal;
