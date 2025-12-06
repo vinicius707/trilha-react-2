0bc49b6418f2edd773f31d2c5b3681525de8e9d6
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 11:08:33 -0300

    Refactor BalancoFinanceiro component to utilize useGastosPorCategoria hook for dynamic expense categorization; add new hook for calculating expenses by category based on transaction data.

---
+++ src/hooks/useGastosPorCategoria.tsx
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import { useAppContext } from "../context/AppContext";
+
+export const useGastosPorCategoria = () => {
+  const { transacoes } = useAppContext();
+
+  const gastosPorCategoria = useMemo(() => {
+    return transacoes
+      .filter((transacao) => transacao.tipo === "despesa")
+      .reduce<Record<string, number>>((total, transacao) => {
+        total[transacao.categoria] =
+          (total[transacao.categoria] || 0) + transacao.valor;
+        return total;
+      }, {});
+  }, [transacoes]);
+  return gastosPorCategoria;
+};
+
+export default useGastosPorCategoria;
